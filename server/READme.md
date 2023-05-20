483fa0a5735204dcb441ceba8a2ef55acf7cce2d
Jeremy Johns <jez.johns@gmail.com>
2023-05-20 12:01:38 +0000

    linked /kanjis endpoint to kanjisalive api

---
+++ server/READme.md
@@ -0,0 +1,5 @@
+kanji alive API  
+returns:  
+for advanced search with params: { grade: "2" },  
+ array of kanji objects: "kanji": { "character": "書", "stroke": 10 }, "radical":
+{ "character": "⽇", "stroke": 4, "order": 92 }
--- server/server.js
+++ server/server.js
@@ -11,26 +11,26 @@ app.use(cors());
 app.get("/", (request, response) => {
 	response.json("Welcome to the root!");
 });
-app.get("/kanjis", (request, response) => {
-	response.json("Hi. You have reached Kanjis. How can I help?");
-});
-// code from rapidapi to get data from API - not quite working
-async function getAPI() {
+app.get("/kanjis", async (request, response) => {
 	const options = {
 		method: "GET",
-		url: "https://kanjialive-api.p.rapidapi.com/api/public/kanji/%E8%A8%AA",
+		url: "https://kanjialive-api.p.rapidapi.com/api/public/search/advanced/",
+		params: { grade: "2" },
 		headers: {
-			"X-RapidAPI-Key": "db5eeaf343msh1a63a5c2feec823p1ae435jsn1ba325299f3d",
+			"X-RapidAPI-Key": process.env.RAPID_API_KEY,
 			"X-RapidAPI-Host": "kanjialive-api.p.rapidapi.com",
 		},
 	};
 
 	try {
 		const res = await axios.request(options);
-		return res.data;
+		response.json(res.data);
 	} catch (error) {
 		console.error(error);
 	}
-}
+	// response.json("Hi. You have reached Kanjis. How can I help?");
+});
+// code from rapidapi to get data from API - not quite working
+async function getAPI() {}
 
 app.listen(PORT, () => console.log("App listening on PAUGHT " + PORT));
