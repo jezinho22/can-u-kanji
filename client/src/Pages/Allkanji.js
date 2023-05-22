642c31c868926c6afd7a17ea80e7923a4c6e235b
Jeremy Johns <jez@TechEd-0019.techeducators.co.uk>
2023-05-22 13:14:55 +0100

    set up pages and compnonets on client

--- client/src/Pages/Allkanji.js
+++ client/src/Pages/Allkanji.js
@@ -1,3 +1,21 @@
+import Dropdown from "react-dropdown"
+
+
 export default function Allkanji() {
-    return <div>Allkanji</div>;
+    const options = ["Grade 1", "Grade 2"]
+    const defaultOption = options[0]
+    
+function handleSelect(event){
+    console.log("handleseelect firing")
+console.log(event.value)
+}
+
+    return (
+    <div>   <h2>All Kanji</h2><Dropdown options={options} 
+                    onChange={handleSelect} 
+                    placeholder="Select grade" 
+                    value={defaultOption}/>
+ 
+    </div>
+    )
 }
