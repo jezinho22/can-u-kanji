d1a1f982ebc3c40ee9929eeefcc1a0947f9e4d0f
Rachael Hindley <rachael.hindley52@gmail.com>
2023-05-23 20:58:19 +0100

    dummy card. Done with inline CSS so looks super cluttered and messy.

--- client/src/Pages/Mykanji.js
+++ client/src/Pages/Mykanji.js
@@ -1,3 +1,38 @@
 export default function Mykanji() {
-    return <div className="sub-heading">Mykanji</div>;
+    return (
+        <div className="sub-heading">
+            <h2 style={{ marginBottom: "1rem", fontSize: "1.5rem" }}>Mykanji</h2>
+
+            <div
+                style={{
+                    backgroundColor: "white",
+                    border: "solid black 1px",
+                    width: "250px",
+                    textAlign: "center",
+                    padding: "0.75rem 0 0 0",
+                    boxShadow: "2px 2px grey",
+                }}
+                className="card"
+            >
+                <h3 style={{ fontSize: "4rem" }}>日</h3>
+                <button>See Stroke Order</button>
+                <p style={{ marginBottom: "0.5rem" }}>
+                    Meaning- <span style={{ backgroundColor: "pink" }}>day</span>; sun
+                </p>
+                <p>
+                    Kunyomi-
+                    <span style={{ fontWeight: "500" }}>
+                        　に,　
+                        <span style={{ backgroundColor: "pink" }}>にち</span>
+                    </span>
+                </p>
+                <p>
+                    Onyomi- <span style={{ fontWeight: "500" }}>び,　ひ,　か</span>
+                </p>
+                <button style={{ marginLeft: "210px", marginTop: "1.5rem" }}>X</button>
+
+                {/* link to video field opens in model? */}
+            </div>
+        </div>
+    );
 }
