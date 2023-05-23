37e186d20b8cf3ba7d33fe2f3b8a6c78283d9d17
Rachael Hindley <rachael.hindley52@gmail.com>
2023-05-23 14:06:32 +0100

    seeding database

---
+++ server/seed.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose.connect(process.env.DATABASE_URL);
+
+const User = require("./models/usermodel");
+
+async function seed() {
+    await User.create(
+        {
+            email: "someone@somewhere",
+            mykanji: [{ character: "x", meaning: "xx" }],
+        },
+        { email: "someoneelse@differentplace", mykanji: [{ character: "y", meaning: "yy" }] }
+    );
+    mongoose.disconnect();
+}
+
+seed();
