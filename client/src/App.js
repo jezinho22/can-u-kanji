74ccebcc6010f21bbfd7c944a31cea151c622f12
Rachael Hindley <rachael.hindley52@gmail.com>
2023-05-23 15:58:49 +0100

    mykanji page getMyKanji and deleteKanji

--- client/src/App.js
+++ client/src/App.js
@@ -9,12 +9,10 @@ import Mykanji from "./Pages/Mykanji";
 import Pairgame from "./Pages/Pairgame";
 import { useState } from "react";
 
-
 function App() {
-
-const [email, setEmail] = useState("")
-    function handleChangeEmail(event){
-        setEmail(event.target.value)
+    const [email, setEmail] = useState("");
+    function handleChangeEmail(event) {
+        setEmail(event.target.value);
     }
 
     return (
@@ -23,8 +21,8 @@ const [email, setEmail] = useState("")
                 <Header />
                 <Routes>
                     <Route path="/" element={<Home />} />
-                    <Route path="/allkanji" element={<Allkanji email={email} handleChangeEmail={handleChangeEmail}/>} />
-                    <Route path="/mykanji" element={<Mykanji />} />
+                    <Route path="/allkanji" element={<Allkanji email={email} handleChangeEmail={handleChangeEmail} />} />
+                    <Route path="/mykanji" element={<Mykanji email={email} />} />
                     <Route path="/pairgame" element={<Pairgame />} />
                 </Routes>
                 <Footer />
--- client/src/Pages/Mykanji.js
+++ client/src/Pages/Mykanji.js
@@ -1,3 +1,24 @@
-export default function Mykanji() {
-    return <div className="sub-heading">Mykanji</div>;
+import axios from "axios";
+import { useState } from "react";
+
+export default function Mykanji({ email }) {
+    const [myKanji, setMyKanji] = useState({});
+
+    async function getMyKanji() {
+        const API = `http://localhost:8077/mykanji/${email}`;
+        const res = await axios.get(API);
+        setMyKanji(res.data);
+    }
+
+    async function deleteKanji(id) {
+        const API = `http://localhost:8077/mykanji/${id}`;
+        await axios.delete(API);
+        getMyKanji();
+    }
+
+    return (
+        <div className="sub-heading">
+            <h2>Mykanji</h2>
+        </div>
+    );
 }
