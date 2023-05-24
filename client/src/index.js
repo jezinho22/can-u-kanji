3c11459b73d713f11ca50ede6470b5fd32950c32
jezinho22 <jez.johns@gmail.com>
2023-05-24 18:15:52 +0100

    going to reset

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <App />
+        <Auth0Provider
+            domain="dev-wq6s47m77sxbmwmj.us.auth0.com"
+            clientId="GudB6RCbgeTYKq8r6VCmh91xyQvtS7yw"
+            authorizationParams={{
+            redirect_uri: window.location.origin
+            }}>
+
+
+        <App /></Auth0Provider>
     </React.StrictMode>
 );
