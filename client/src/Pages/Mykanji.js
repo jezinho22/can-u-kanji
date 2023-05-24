fb502834c3bcdb3f21e0fb7b93a95b4eb79ace6f
Rachael Hindley <rachael.hindley52@gmail.com>
2023-05-24 13:59:08 +0100

    my kanji

--- client/src/Pages/Mykanji.js
+++ client/src/Pages/Mykanji.js
@@ -1,13 +1,15 @@
 import axios from "axios";
 import { useState } from "react";
 
-export default function Mykanji({ email }) {
+export default function Mykanji() {
     const [myKanji, setMyKanji] = useState({});
+    const [email, setEmail] = useState("jez.johns@email");
 
     async function getMyKanji() {
-        console.log(myKanji)
-        const API = `https://can-u-kanji.onrender.com/mykanji/${email}`;
+        console.log(myKanji);
+        const API = `https://can-u-kanji.onrender.com/mykanji/?email=${email}`;
         const res = await axios.get(API);
+        console.log(res.data);
         setMyKanji(res.data);
     }
 
@@ -20,7 +22,19 @@ export default function Mykanji({ email }) {
     return (
         <div className="sub-heading">
             <h2>Mykanji</h2>
-            <button onClick={deleteKanji}>This is not really the button</button>
+            <div className="card">
+                <h3 style={{ fontSize: "4rem" }}>日</h3>
+                <button>See Stroke Order</button>
+                <p>Meaning- day; sun</p>
+                <p>Kunyomi- 　に,　にち</p>
+                <p>Onyomi- び,　ひ,　か</p>
+                <button style={{ marginLeft: "210px", marginTop: "1.5rem" }} onClick={deleteKanji}>
+                    Remove
+                </button>
+                <button style={{ marginLeft: "210px", marginTop: "1.5rem" }} onClick={getMyKanji}>
+                    Get My Kanji
+                </button>
+            </div>
         </div>
     );
 }
