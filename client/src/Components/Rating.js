e94d1d000108e3c80dc1196eebcd6e109173340e
Jeremy Johns <jez.johns@gmail.com>
2023-08-28 15:47:56 +0000

    annotated rating stars

--- client/src/Components/Rating.js
+++ client/src/Components/Rating.js
@@ -1,25 +1,31 @@
-import { useState } from 'react'
+import { useState } from "react";
+import "../css/Rating.css";
 
-export default function Rating({rating, handleRating, kanji}) {
+export default function Rating({ rating, handleRating, kanji }) {
+	const [hover, setHover] = useState(0);
 
-    const [hover, setHover] = useState(0)
-
-    return (
-        <div className="star-rating">
-          {[...Array(5)].map((star, index) => {
-            index+=1;
-            return (                       
-                <button type="button" 
-                        key={index} 
-                        className={index <= (hover || kanji.rating) ? "on" : "off"} 
-                        onClick={()=> handleRating(index, kanji._id)}
-                        onMouseEnter={()=>setHover(index)}
-                        onMouseLeave={()=>setHover(kanji.rating)}>
-                <span className="star">&#9733;</span>
-                </button>        
-            );
-          })}
-        </div>
-      );
-  
+	return (
+		<div className="star-rating">
+			{/* display stars - not yet imported */}
+			{[...Array(5)].map((star, index) => {
+				index += 1;
+				// for each item return a button with multiple functions
+				return (
+					<button
+						type="button"
+						key={index}
+						// set up css selector for stars lighting up - either as before or higher
+						className={index <= (hover || kanji.rating) ? "on" : "off"}
+						// set the rating on click
+						onClick={() => handleRating(index, kanji._id)}
+						// set how stars light up when mouse comes over
+						onMouseEnter={() => setHover(index)}
+						onMouseLeave={() => setHover(kanji.rating)}>
+						{/* create star icon as button */}
+						<span className="star">&#9733;</span>
+					</button>
+				);
+			})}
+		</div>
+	);
 }
